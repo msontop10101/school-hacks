fdc48f7cea9aad381d0b272ce140c34d90031d9f
Moyosore Elijah <msontop.me@gmail.com>
2023-01-23 12:18:15 -0800

    final commit on school-hacks0.1

--- src/App.js
+++ src/App.js
@@ -15,16 +15,16 @@ import email from './assets/images/email.svg'
 
 function App() {
   return (
-  <div>
-    {/* SECTION ONE */}
+    <div>
+      {/* SECTION ONE */}
       <div class='bg-[#9E9E9E] h-[70px] md:h-[100px] flex items-center px-[30px] md:px-28'>
-      <div><a href='/'><img src={logo} height='70px' width='70px' alt='school-hacks-logo'/></a></div>
+        <div><a href='/'><img src={logo} height='70px' width='70px' alt='school-hacks-logo' /></a></div>
       </div>
-    {/* SECTION TWO */}
-      <div class='py-10 px-4 md:px-28 flex flex-col md:flex-row items-center firstbackground'>
-      <div class='md:hidden'>
+      {/* SECTION TWO */}
+      <div class='py-10 px-4 md:px-28 flex flex-col md:flex-row items-center'>
+        <div class='md:hidden'>
           <div>
-            <img src={notebooks} width='300px' height='300px' alt='notebook-imgage'/>
+            <img src={notebooks} width='300px' height='300px' alt='notebook-imgage' />
           </div>
         </div>
         <div class='font-semibold w-full md:w-[70%]'>
@@ -34,11 +34,11 @@ function App() {
         </div>
         <div class='hidden md:block'>
           <div>
-            <img src={notebooks} alt='notebook-imgage'/>
+            <img src={notebooks} alt='notebook-imgage' />
           </div>
         </div>
       </div>
-      
+
       {/* SECTION THREE */}
       <div class='bg-[#9E9E9E] text-center px-4 md:px-[120px] pb-4 md:pb-[100px] pt-4 md:pt-[40px]'>
         <h2 class='font-bold text-[40px] text-white uppercase'>A props</h2>
@@ -46,87 +46,87 @@ function App() {
       </div>
       {/* SECTION FOUR */}
       <div>
-        <div class='py-10 background'>
+        <div class='py-10'>
           <h2 class='font-bold text-[40px] text-center mb-10'>NOS SERVICES</h2>
           <div class='flex justify-center'>
-          <div>
-          <div class='flex flex-col items-center md:flex-row gap-2 md:gap-10 mb-2 md:mb-10 justify-center'>
-            <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
-              <div class='mr-2'><img src={one} width='160px' height='160px' alt='one'/></div>
-              <div><p class='font-semibold'>This is tag number one! This is tag number one!</p></div>
+            <div>
+              <div class='flex flex-col items-center md:flex-row gap-2 md:gap-10 mb-2 md:mb-10 justify-center'>
+                <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
+                  <div class='mr-2'><img src={one} width='160px' height='160px' alt='one' /></div>
+                  <div><p class='font-semibold'>This is tag number one! This is tag number one!</p></div>
+                </div>
+                <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
+                  <div class='mr-2'><img src={two} width='120px' height='120px' alt='two' /></div>
+                  <div><p class='font-semibold'>This is tag number two!</p></div>
+                </div>
+              </div>
+              <div class='flex flex-col items-center md:flex-row gap-2 md:gap-10 mb-2 md:mb-10 justify-center'>
+                <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
+                  <div class='mr-4'><img src={three} width='100px' height='100px' alt='three' /></div>
+                  <div><p class='font-semibold'>This is tag number one!</p></div>
+                </div>
+                <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
+                  <div class='mr-4'><img src={four} width='100px' height='100px' alt='four' /></div>
+                  <div><p class='font-semibold'>This is tag number two!</p></div>
+                </div>
+              </div>
+              <div class='flex gap-10 justify-center'>
+                <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
+                  <div class='mr-4'><img src={five} width='100px' height='100px' alt='five' /></div>
+                  <div><p class='font-semibold'>This is tag number one!</p></div>
+                </div>
+              </div>
             </div>
-            <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
-              <div class='mr-2'><img src={two} width='120px' height='120px' alt='two'/></div>
-              <div><p class='font-semibold'>This is tag number two!</p></div>
-            </div>
-          </div>
-          <div class='flex flex-col items-center md:flex-row gap-2 md:gap-10 mb-2 md:mb-10 justify-center'>
-            <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
-              <div class='mr-4'><img src={three} width='100px' height='100px' alt='three'/></div>
-              <div><p class='font-semibold'>This is tag number one!</p></div>
-            </div>
-            <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
-              <div class='mr-4'><img src={four} width='100px' height='100px' alt='four'/></div>
-              <div><p class='font-semibold'>This is tag number two!</p></div>
-            </div>
-          </div>
-          <div class='flex gap-10 justify-center'>
-            <div class='bg-[#D2D2D2] rounded-2xl flex items-center p-5 h-[120px] w-[90%] md:w-[40%]'>
-              <div class='mr-4'><img src={five} width='100px' height='100px' alt='five'/></div>
-              <div><p class='font-semibold'>This is tag number one!</p></div>
-            </div>
-          </div>
-          </div>
           </div>
         </div>
         <div class='w-full'>
-        
+
         </div>
       </div>
       {/* SECTION FIVE */}
       <div class=' bg-[#9E9E9E] px-0 md:px-28'>
         <div class='w-full flex items-center'>
-        <div class='text-white flex justify-center py-10'>
-          <div class=' w-full md:w-[90%]'>
-          <h1 class='text-[20px] md:text-[40px] font-bold mb-2 text-center'>This is the header</h1>
-          <h4 class=' text-[20px] md:text-[40px] font-normal text-center'>Another small heading</h4>
-          <p class='text-center'> type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
-        
-        <div class='py-5 px-2 md:px-20'>
-        <RegistrationForm/>
-        </div>
-        <p class='text-center'> type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
-        
-            </div>  
-        </div>
+          <div class='text-white flex justify-center py-10'>
+            <div class=' w-full md:w-[90%]'>
+              <h1 class='text-[20px] md:text-[40px] font-bold mb-2 text-center'>This is the header</h1>
+              <h4 class=' text-[20px] md:text-[40px] font-normal text-center'>Another small heading</h4>
+              <p class='text-center'> type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
+
+              <div class='py-5 px-2 md:px-20'>
+                <RegistrationForm />
+              </div>
+              <p class='text-center'> type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets </p>
+
+            </div>
+          </div>
         </div>
         <div>
           <footer class='flex flex-col md:flex-row justify-between py-14'>
             <div class='w-[50%] md:w-[20%] pl-4 md:pl-0'>
-              <div class='flex justify-center'><a href='/'><img src={logo} width='110px' height='110px' alt='logo'/></a></div>
+              <div class='flex justify-center'><a href='/'><img src={logo} width='110px' height='110px' alt='logo' /></a></div>
               <p class='font-semibold flex justify-center'>studentshacks.com</p>
             </div>
 
 
             {/* --------DESKTOP-ICONS------------ */}
             <div class='hidden md:flex flex-col md:flex-row w-full md:w-[60%] justify-evenly'>
-              
+
               <div class='mr-10 flex-col items-center'>
-              <div class='flex justify-center'><a href='https://web.facebook.com/people/Student-Hacks/100089567025384/'><img src={facebook} width='60px' height='60px' alt="facebook"/></a></div>
-              <div><p class='font-semibold'>Student Hacks</p></div>
+                <div class='flex justify-center'><a href='https://web.facebook.com/people/Student-Hacks/100089567025384/'><img src={facebook} width='60px' height='60px' alt="facebook" /></a></div>
+                <div><p class='font-semibold'>Student Hacks</p></div>
               </div>
               <div class='mr-10'>
-              <div class='flex justify-center'><a href='https://twitter.com/StudentHacks0'><img src={twitter} width='60px' height='60px' alt="twitter"/></a></div>
-              <p class='font-semibold'>Student Hacks</p>
+                <div class='flex justify-center'><a href='https://twitter.com/StudentHacks0'><img src={twitter} width='60px' height='60px' alt="twitter" /></a></div>
+                <p class='font-semibold'>Student Hacks</p>
               </div>
 
               <div class='mr-10'>
-              <div class='flex justify-center'><a href='linkedin.com'><img src={linkedin} width='60px' height='60px' alt="linkedin"/></a></div>
-              <p class='font-semibold'>Student Hacks</p>
+                <div class='flex justify-center'><a href='linkedin.com'><img src={linkedin} width='60px' height='60px' alt="linkedin" /></a></div>
+                <p class='font-semibold'>Student Hacks</p>
               </div>
               <div>
-              <div class='flex justify-center'><a href='mailto:contact.studenthacks@gmail.com'><img src={email} width='60px' height='60px' alt="email"/></a></div>
-              <p class='font-semibold'>contact@votresite.come</p>
+                <div class='flex justify-center'><a href='mailto:contact.studenthacks@gmail.com'><img src={email} width='60px' height='60px' alt="email" /></a></div>
+                <p class='font-semibold'>contact@votresite.come</p>
               </div>
 
             </div>
@@ -136,27 +136,27 @@ function App() {
             <div>
               <div class='pt-4 block md:hidden'>
 
-              <div class='flex justify-center gap-10 mb-4'>
-              <div class='flex flex-col items-center'>
-              <div class=''><a href='https://web.facebook.com/people/Student-Hacks/100089567025384/'><img src={facebook} width='60px' height='60px' alt="facebook"/></a></div>
-              <div><p class='font-semibold'>Student Hacks</p></div>
-              </div>
-              <div class='flex flex-col items-center'>
-              <div><a href='https://twitter.com/StudentHacks0'><img src={twitter} width='60px' height='60px' alt="twitter"/></a></div>
-              <div><p class='font-semibold'>Student Hacks</p></div>
-              </div>
-              </div>
+                <div class='flex justify-center gap-10 mb-4'>
+                  <div class='flex flex-col items-center'>
+                    <div class=''><a href='https://web.facebook.com/people/Student-Hacks/100089567025384/'><img src={facebook} width='60px' height='60px' alt="facebook" /></a></div>
+                    <div><p class='font-semibold'>Student Hacks</p></div>
+                  </div>
+                  <div class='flex flex-col items-center'>
+                    <div><a href='https://twitter.com/StudentHacks0'><img src={twitter} width='60px' height='60px' alt="twitter" /></a></div>
+                    <div><p class='font-semibold'>Student Hacks</p></div>
+                  </div>
+                </div>
 
-              <div class='flex justify-center gap-10'>
-              <div class='flex flex-col items-center'>
-              <div><a href='linkedin.com'><img src={linkedin} width='60px' height='60px' alt="linkedin"/></a></div>
-              <div><p class='font-semibold'>Student Hacks</p></div>
-              </div>
-              <div  class='flex flex-col items-center'>
-              <div><a href='mailto:contact.studenthacks@gmail.com'><img src={email} width='60px' height='60px' alt="email"/></a></div>
-              <div><p class='font-semibold'>Student Hacks</p></div>
-              </div>
-              </div>
+                <div class='flex justify-center gap-10'>
+                  <div class='flex flex-col items-center'>
+                    <div><a href='linkedin.com'><img src={linkedin} width='60px' height='60px' alt="linkedin" /></a></div>
+                    <div><p class='font-semibold'>Student Hacks</p></div>
+                  </div>
+                  <div class='flex flex-col items-center'>
+                    <div><a href='mailto:contact.studenthacks@gmail.com'><img src={email} width='60px' height='60px' alt="email" /></a></div>
+                    <div><p class='font-semibold'>Student Hacks</p></div>
+                  </div>
+                </div>
               </div>
             </div>
             {/* ------------MOBILE----------- */}
